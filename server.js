0be27d902388002542832a47dda0dae0f29d2f29
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-13 13:36:17 +0200

    Setup of server with socket

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'view')));
+
+io.on("connection", socket => {
+    socket.emit("message", "Welcome to ChatCod!");
+
+    socket.broadcast.emit("message", "A user has joined the chat");
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left the chat");
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server runing on port http://localhost:${PORT}/`));
