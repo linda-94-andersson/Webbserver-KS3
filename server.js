012d563d69c2b8598a23a7a09525b60a3ed89362
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-21 11:48:02 +0200

    Dynamic setup for messages

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,13 +11,38 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'view')));
 
-io.on("connection", socket => {
-    socket.emit("message", "Welcome to ChatCod!");
+const admin = "Admin";
 
-    socket.broadcast.emit("message", "A user has joined the chat");
+io.on("connection", socket => {
+    socket.on("joinRoom", ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
+        socket.emit("message", formatMessage(admin, "Welcome to ChatCod!"));
+
+        socket.broadcast.to(user.room).emit("message", formatMessage(admin, `${user.username} has joined the chat`));
+
+        io.to(user.room).emit("roomUsers", {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    });
+
+    socket.on("chatMessage", (msg) => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit("message", formatMessage(user.username, msg));
+    });
 
     socket.on("disconnect", () => {
-        io.emit("message", "A user has left the chat");
+        const user = userLeave(socket.id);
+        if (user) {
+            io.to(user.room).emit("message", formatMessage(admin, `${user.username} has left the chat`));
+            io.to(user.room).emit("roomUsers", {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
     });
 });
 
