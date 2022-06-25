eae08a73dee598134450eabc4b558d2a7bf17dc9
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-25 16:39:57 +0200

    Added models

---
+++ models/rooms.model.js
@@ -0,0 +1,49 @@
+const db = require("../config/db");
+
+function createRoom(id, room) {
+    const sql = "INSERT INTO rooms (id, room) VALUES (?,?)";
+
+    return new Promise((resolve, reject) => {
+        db.run(sql, [id, room], (error, rooms) => {
+            if (error) {
+                console.error(error.message);
+                reject(error);
+            }
+            resolve(rooms);
+        });
+    });
+}
+
+function getOneRoom(id) {
+    const sql = "SELECT * FROM rooms WHERE id = ?";
+
+    return new Promise((resolve, reject) => {
+        db.get(sql, id, function (error, room) {
+            if (error) {
+                console.error(error.message);
+                reject(error);
+            }
+            resolve(room);
+        });
+    });
+}
+
+function deleteRoom(id) {
+    const sql = "DELETE FROM rooms WHERE id = ?";
+
+    return new Promise((resolve, reject) => {
+        db.run(sql, id, (error) => {
+            if (error) {
+                console.error(error.message);
+                reject(error);
+            }
+            resolve(id);
+        });
+    });
+}
+
+module.exports = {
+    createRoom,
+    getOneRoom,
+    deleteRoom
+}
